159eda22810d1f8cb40dbaefbf702f9f97a5f1c7
Carter Supple <Cartersupple123@gmail.com>
2021-10-28 15:20:09 -0700

    added js for storing city names to local storage and a button to generate weather data from city name

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-header {
-    background-image: linear-gradient(to right, rgba(102, 126, 234, 0.5), rgba(118, 75, 162, 0.5));
+var cityList = [];
+var APIkey = "c6d0a2598a833c465f90853cbcfc28ec";
+// stores searched cities to localStorage
+function storeCity() {
+    localStorage.setItem("city", JSON.stringify(cityList));
+}
+// creates a button to click to generate weather data for an entered city
+function citylist() {
+    $(".cityList").empty();
+    cityList.forEach(function(city){
+        $(".cityList").prepend($(`<button class="list-group-item list-group-item-action cityBtn" data-city="${city}">${city}</button>`));
+    })
 }
